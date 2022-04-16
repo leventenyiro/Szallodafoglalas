bfec724e2fed56760f5beeaa04320e37909f5075
Nyírő Levente Gyula <b4kzan@inf.elte.hu>
2022-04-16 10:02:28 +0200

    feat(models): add

---
+++ Szallodafoglalas/Models/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szallodafoglalas.Models
+{
+    internal class Hotel
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public int OneBed { get; }
+        public int TwoBed { get; }
+
+        public Hotel(string id, string name, int oneBed, int twoBed)
+        {
+            Id = id;
+            Name = name;
+            OneBed = oneBed;
+            TwoBed = twoBed;
+        }
+    }
+}
---
+++ Szallodafoglalas/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szallodafoglalas.Models
+{
+    internal class Reservation
+    {
+        public string Id { get; set; }
+        public string HotelId { get; }
+        public int Bed { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Tel { get; }
+        public DateTime Date { get; }
+
+        public Reservation(string id, string hotelId, int bed, string name, string email, string tel, DateTime date)
+        {
+            Id = id;
+            HotelId = hotelId;
+            Bed = bed;
+            Name = name;
+            Email = email;
+            Tel = tel;
+            Date = date;
+        }
+    }
+}
