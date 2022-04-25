4e175cf6e08a748d4c218fcadbbe1e89a9643d39
Nyírő Levente Gyula <b4kzan@inf.elte.hu>
2022-04-25 22:42:24 +0200

    feat(hoteldb): connection string to app.config

--- Szallodafoglalas/AutoDir/HotelDb.cs
+++ Szallodafoglalas/AutoDir/HotelDb.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Szallodafoglalas;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using Szallodafoglalas.Models;
 
 namespace Szallodafoglalas.AutoDir
@@ -25,8 +22,8 @@ namespace Szallodafoglalas.AutoDir
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\temp\\hotel.mdf;Integrated Security=True;Connect Timeout=30");
+                var connStr = ConfigurationManager.AppSettings.Get("ConnectionString");
+                optionsBuilder.UseSqlServer(connStr);
             }
         }
 
